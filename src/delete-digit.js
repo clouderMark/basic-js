5e4d72cd5cd052e67e2b79a2f0252bf5b3e78c09
clouderMark <charnysheuski@yandex.by>
2022-10-13 17:07:25 +0300

    feat: a few more

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  n = n.toString().split('')
+  let n1 = n.slice(0)
+  let n2 = n.slice(0)//этот по возрастанию
+
+  n2.sort((a, b) => a - b)
+  n1.splice(n1.indexOf(n2[0]), 1)
+  n.splice(n.indexOf(n2[1]), 1)
+ return n > n1 ? +n.join('') : +n1.join('')
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+
+  let sum = (num) => {
+    return num.toString().split('').reduce((acc, i) => +acc + +i)
+  }
+
+  while (n > 9) {
+    n = sum(n)
+  }
+  return n
 }
 
 module.exports = {
